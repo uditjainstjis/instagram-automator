5b3e0fe785fcc868bd31d2b623566dea978bc3e9
Udit Jain <uditjainstjis@gmail.com>
2026-03-04 12:19:50 +0530

    Added path module import

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const cookieSession = require('cookie-session');
